9dfe1c62c19fd4bac5aa9c44d8a4e66c8b7b0089
Darin Kotter <darin.kotter@my.eitc.edu>
2015-02-27 11:14:30 -0700

    Make sure we are in the right directories when running commands.

--- prepare.sh
+++ prepare.sh
@@ -17,7 +17,7 @@ wget -nv -O tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/
 mkdir -p $WP_CORE_DIR
 tar --strip-components=1 -zxmf tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
-tests=$(basename $(pwd))
+tests=$(basename $(pwd))/tests
 mv tests $WP_CORE_DIR
 
 composer create-project wp-cli/wp-cli $WP_CORE_DIR/wp-cli --no-dev
@@ -27,3 +27,5 @@ cd $WP_CORE_DIR
 wp-cli/bin/wp core config --dbname=wordpress_test --dbuser=root --dbhost=localhost
 wp-cli/bin/wp core install --url=http://example.com --title="Just another test site" --admin_user=wordpress --admin_password=password --admin_email=test@example.com
 curl http://loripsum.net/api/5 | wp-cli/bin/wp post generate --post_content --count=30
+
+cd ../..
--- tests.sh
+++ tests.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+cd /tmp/wordpress
+
 php tests/post-not-in-tests.php
