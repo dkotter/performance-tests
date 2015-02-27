a5fdb9a5bba9984ee2ace62e8b206bed2c39b01d
Darin Kotter <darin.kotter@my.eitc.edu>
2015-02-27 09:58:14 -0700

    Change tests to use the WP unit test framework.

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-# called by Travis CI
+# WordPress test setup script for Travis CI 
+#
+# Author: Benjamin J. Balter ( ben@balter.com | ben.balter.com )
+# License: GPL3
 
 export WP_CORE_DIR=/tmp/wordpress
 export WP_TESTS_DIR=/tmp/wordpress-tests
@@ -17,3 +20,12 @@ tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 svn co --quiet --ignore-externals http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
 
 wget -nv -O $WP_TESTS_DIR/wp-tests-config.php https://raw.github.com/tierra/wordpress-plugin-tests/setup/wp-tests-config.php
+
+# Put various components in proper folders
+plugin_slug=$(basename $(pwd))
+plugin_dir=$WP_CORE_DIR/wp-content/plugins/$plugin_slug
+
+cd ..
+mv $plugin_slug $plugin_dir
+
+cd $plugin_dir
