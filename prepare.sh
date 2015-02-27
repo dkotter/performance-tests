497be20f0f5c0f2e174ddbdc1e6efec28e99e6b2
Darin Kotter <darin.kotter@my.eitc.edu>
2015-02-27 10:07:37 -0700

    Modify our prepare script a bit.

--- prepare.sh
+++ prepare.sh
@@ -5,6 +5,8 @@
 # Author: Benjamin J. Balter ( ben@balter.com | ben.balter.com )
 # License: GPL3
 
+mkdir -p tmp
+
 export WP_CORE_DIR=/tmp/wordpress
 export WP_TESTS_DIR=/tmp/wordpress-tests
 
@@ -12,12 +14,12 @@ export WP_TESTS_DIR=/tmp/wordpress-tests
 mysql -e 'CREATE DATABASE wordpress_test;' -uroot
 
 # Grab specified version of WordPress from github
-wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
+wget -nv -O tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
 mkdir -p $WP_CORE_DIR
-tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+tar --strip-components=1 -zxmf tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
 # Grab testing framework and config file
-svn co --quiet --ignore-externals http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
+svn co --ignore-externals http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
 
 wget -nv -O $WP_TESTS_DIR/wp-tests-config.php https://raw.github.com/tierra/wordpress-plugin-tests/setup/wp-tests-config.php
 
