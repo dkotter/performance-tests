fc675afc6b8f8c041a45be1e9dcfa6a66320f2af
Darin Kotter <darin.kotter@my.eitc.edu>
2015-03-16 14:25:54 -0600

    Bring grouping over to the base test class.

--- tests/kses.php
+++ tests/kses.php
@@ -18,9 +18,28 @@ class Kses_Tests extends Tests {
 	 *
 	 * @param int $n Number of tests to run.
 	 * @param string $test Test to run.
+	 * @param string $group Name of group test is part of.
 	 * @return void
 	 */
-	public function run_test( $n, $test ) {
+	public function run_test( $num, $test, $group ) {
+		$p_tags = array(
+			'a'      => array(
+				'href',
+				'class',
+				'id',
+				'target',
+				'title',
+				'alt',
+			),
+			'span'   => array(
+				'class',
+				'id',
+			),
+			'em'     => array(),
+			'strong' => array(),
+			'i'      => array(),
+			'b'      => array(),
+		);
 		$query_args = array(
 			'post_type'      => 'post',
 			'post_status'    => 'publish',
@@ -30,33 +49,15 @@ class Kses_Tests extends Tests {
 		$post = $query->posts[0];
 		$post_content = $post->post_content;
 
-		for ( $i = 0; $i < $n; $i++ ) {
+		for ( $i = 0; $i < $num; $i++ ) {
 			$start = $this->start_timer();
 			if ( 'wp_kses' === $test ) {
-				$p_tags = array(
-					'a'      => array(
-						'href',
-						'class',
-						'id',
-						'target',
-						'title',
-						'alt',
-					),
-					'span'   => array(
-						'class',
-						'id',
-					),
-					'em'     => array(),
-					'strong' => array(),
-					'i'      => array(),
-					'b'      => array(),
-				);
 				$test( $post_content, $p_tags );
 			} else {
 				$test( $post_content );
 			}
 			$stop = $this->stop_timer();
-			$this->add_result( $test, $this->get_elapsed_time( $start, $stop ) );
+			$this->add_result( $group, $test, $this->get_elapsed_time( $start, $stop ) );
 		}
 	}
 
